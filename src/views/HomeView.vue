11169a77fb350298cf4ed4890ec28c15ecf13e0e
Teiden <matteo.knogler@gmail.com>
2024-10-15 12:35:26 +0200

    Changed the text of all the boxes to a lorem ipsum paragraph and changed their headings to be "box n"

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,38 +6,80 @@
   <div class="page">
     <div class="contain left">
       <div class="cont">
-        <h2>Why use grocery list?</h2>
-        <p>asdfasdf</p>
+        <h2>Box 1</h2>
+        <p>
+          Velit eiusmod eu commodo mollit magna irure veniam adipisicing Lorem duis labore do ut in.
+          Est id labore excepteur veniam magna culpa do magna anim proident occaecat. Ea excepteur
+          occaecat est cillum aliqua ut sunt amet elit. Ipsum aute proident non in. Aliqua deserunt
+          labore cupidatat labore ad enim commodo. Proident ullamco aliqua labore esse consectetur
+          sit ullamco ea incididunt cupidatat. Occaecat proident reprehenderit excepteur laborum
+          aliquip sit magna nostrud.
+        </p>
       </div>
     </div>
     <div class="contain right">
       <div class="cont">
-        <h2>Because</h2>
-        <p>Lorem ipsum..</p>
+        <h2>Box 2</h2>
+        <p>
+          Velit eiusmod eu commodo mollit magna irure veniam adipisicing Lorem duis labore do ut in.
+          Est id labore excepteur veniam magna culpa do magna anim proident occaecat. Ea excepteur
+          occaecat est cillum aliqua ut sunt amet elit. Ipsum aute proident non in. Aliqua deserunt
+          labore cupidatat labore ad enim commodo. Proident ullamco aliqua labore esse consectetur
+          sit ullamco ea incididunt cupidatat. Occaecat proident reprehenderit excepteur laborum
+          aliquip sit magna nostrud.
+        </p>
       </div>
     </div>
     <div class="contain left">
       <div class="cont">
-        <h2>Why use grocery list?</h2>
-        <p>asdfasdf</p>
+        <h2>Box 3</h2>
+        <p>
+          Velit eiusmod eu commodo mollit magna irure veniam adipisicing Lorem duis labore do ut in.
+          Est id labore excepteur veniam magna culpa do magna anim proident occaecat. Ea excepteur
+          occaecat est cillum aliqua ut sunt amet elit. Ipsum aute proident non in. Aliqua deserunt
+          labore cupidatat labore ad enim commodo. Proident ullamco aliqua labore esse consectetur
+          sit ullamco ea incididunt cupidatat. Occaecat proident reprehenderit excepteur laborum
+          aliquip sit magna nostrud.
+        </p>
       </div>
     </div>
     <div class="contain right">
       <div class="cont">
-        <h2>Because</h2>
-        <p>Lorem ipsum..</p>
+        <h2>Box 4</h2>
+        <p>
+          Velit eiusmod eu commodo mollit magna irure veniam adipisicing Lorem duis labore do ut in.
+          Est id labore excepteur veniam magna culpa do magna anim proident occaecat. Ea excepteur
+          occaecat est cillum aliqua ut sunt amet elit. Ipsum aute proident non in. Aliqua deserunt
+          labore cupidatat labore ad enim commodo. Proident ullamco aliqua labore esse consectetur
+          sit ullamco ea incididunt cupidatat. Occaecat proident reprehenderit excepteur laborum
+          aliquip sit magna nostrud.
+        </p>
       </div>
     </div>
     <div class="contain left">
       <div class="cont">
-        <h2>Why use grocery list?</h2>
-        <p>asdfasdf</p>
+        <h2>Box 5</h2>
+        <p>
+          Velit eiusmod eu commodo mollit magna irure veniam adipisicing Lorem duis labore do ut in.
+          Est id labore excepteur veniam magna culpa do magna anim proident occaecat. Ea excepteur
+          occaecat est cillum aliqua ut sunt amet elit. Ipsum aute proident non in. Aliqua deserunt
+          labore cupidatat labore ad enim commodo. Proident ullamco aliqua labore esse consectetur
+          sit ullamco ea incididunt cupidatat. Occaecat proident reprehenderit excepteur laborum
+          aliquip sit magna nostrud.
+        </p>
       </div>
     </div>
     <div class="contain right">
       <div class="cont">
-        <h2>Because</h2>
-        <p>Lorem ipsum..</p>
+        <h2>Box 6</h2>
+        <p>
+          Velit eiusmod eu commodo mollit magna irure veniam adipisicing Lorem duis labore do ut in.
+          Est id labore excepteur veniam magna culpa do magna anim proident occaecat. Ea excepteur
+          occaecat est cillum aliqua ut sunt amet elit. Ipsum aute proident non in. Aliqua deserunt
+          labore cupidatat labore ad enim commodo. Proident ullamco aliqua labore esse consectetur
+          sit ullamco ea incididunt cupidatat. Occaecat proident reprehenderit excepteur laborum
+          aliquip sit magna nostrud.
+        </p>
       </div>
     </div>
   </div>
